535bb85306e8e1676dc9a204c334de4b6a931bfa
waterup <hand.tm@icloud.com>
2022-10-03 00:34:12 +0900

    ブログ投稿の削除処理実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -18,10 +18,25 @@
                     <p class='body'>{{ $post->body }}</p>
                 </div>
                 <a href="/posts/{{ $post->id }}/edit">edit</a>
+                <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                @csrf
+                @method('DELETE')
+                <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+                </form>
             @endforeach
         </div>
         </div class='paginate'>
             {{ $posts->links() }}
         </div>
+        
+        <script>
+        function deletePost(id) {
+            'use strict'
+            
+            if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                document.getElementById(`form_${id}`).submit();
+            }
+        }
+        </script>
     </body>
 </html>
