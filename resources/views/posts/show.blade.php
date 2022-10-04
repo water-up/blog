ece250e667ad2d48d66e51677da5ff385f555897
waterup <hand.tm@icloud.com>
2022-10-05 08:34:01 +0900

    リレーションの実装2

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -18,6 +18,7 @@
             </div>
         </div>
         <div class="footer">
+            <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
             <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
             <a href="/">戻る</a>
         </div>
